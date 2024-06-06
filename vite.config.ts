56c05256e456ce08b1e60075a808ce754b53cb68
EddyShimwa <shedpray16@gmail.com>
2024-06-06 16:38:45 +0200

    set up Ci

--- vite.config.ts
+++ vite.config.ts
@@ -7,10 +7,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
+  server: {
     host: true,
     strictPort: true,
-    port: 8080
+    port: 8080,
   },
   test: {
     globals: true,
