8f8bbd4f919fc332769414cad2ef4bdc7e2b9a82
jkarenzi <j.karenzi@alustudent.com>
2024-06-20 16:43:02 +0200

    chore(tailwind-config): configure tailwind
    - add custom colors in tailwind config
    -add breakpoints and font families
    
    [Finishes #34]

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,29 @@
-/** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    screens: {
+      xs: '320px',
+      sm: '640px',
+      md: '768px',
+      lg: '1440px',
+    },
+    extend: {
+      colors: {
+        footerGray: '#FAFAFB',
+        yellowBg: '#F3C63F',
+        white: '#FFFFFF',
+        violeteBg: '#F5F1FE',
+        blueBg: '#15ABFF',
+        primary: '#6D31ED',
+        grayDark: '#CCD0D8',
+        grayLight: '#F3F4F6',
+        redBg: '#DC2627',
+        black: '#171A1F',
+      },
+      fontFamily: {
+        Lexend: ['Lexend'],
+      }
+    },
   },
   plugins: [],
 };
