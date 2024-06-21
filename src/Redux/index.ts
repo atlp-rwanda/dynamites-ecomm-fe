98ee85d6bc31996289b6aa2cd8cf1322455bfdbc
13XAVI <tresorxavier16@gmail.com>
2024-06-21 10:46:58 +0200

    Configure Redux

--- src/Redux/index.ts
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from '@reduxjs/toolkit';
-import ecommerceReducer from './EcommerceReducer';
-
-const rootReducer = combineReducers({
-  ecommerce: ecommerceReducer,
-});
-
-export default rootReducer;
