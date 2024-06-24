1bb153843c02e63168703b07788547132b6c15b8
Joslyn Manzi Karenzi <j.karenzi@alustudent.com>
2024-06-24 10:40:15 +0200

    feat(navbar): implement navbar (#44)
    -implement reusable navbar component
    
    [Delivers #21]

---
+++ src/__test__/App.test.tsx
@@ -0,0 +1,17 @@
+import { render, screen } from '@testing-library/react';
+import { describe, it, expect } from 'vitest';
+import ErrorPage from '@/pages/ErrorPage';
+
+describe('App', () => {
+  // it('Renders Welcome to Dynamites E-commerce', () => {
+  //   render(<Home />);
+  //   expect(screen.getByRole('paragraph')).toHaveTextContent(
+  //     'Welcome to Dynamites E-commerce'
+  //   );
+  // });
+
+  it('it renders Not Found Page', () => {
+    render(<ErrorPage />);
+    expect(screen.getByRole('paragraph')).toHaveTextContent('Not Found Page');
+  });
+});
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,21 @@
 import CategoriesSection from '@/components/home/categories';
 import HelloSection from '../components/HelloSection/HelloSection';
+import BannerSection from '@/components/bannerAds/bannerSection';
 
 function Home() {
   return (
-    <main className=" relative w-full h-auto p-2 bg-violeteBg lg:p-10 md:p-10">
-      <div>
+    <main>
+      <div className="relative w-full bg-violeteBg h-auto p-2 lg:pl-10 lg:pr-10 lg:pt-10 lg:pb-0 md:pl-10 md:pt-10 md:pr-10 md:pb-0 ">
         <HelloSection />
       </div>
       <div>
         <CategoriesSection />
       </div>
+      <div className="flex w-full h-auto p-8 ">
+        <BannerSection />
+      </div>
+      {/* Add more componets as you wish!!! */}
     </main>
   );
 }
-
 export default Home;
